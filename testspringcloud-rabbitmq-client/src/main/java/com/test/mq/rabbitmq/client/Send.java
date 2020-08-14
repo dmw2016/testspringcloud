85541cbd86a65e1751c481d204c0817353173426
dmw2016 <mingwang.dong@pactera.com>
2020-08-14 13:32:56 +0800

    测试项目；

--- testspringcloud-rabbitmq-client/src/main/java/com/test/mq/rabbitmq/client/Send.java
+++ testspringcloud-rabbitmq-client/src/main/java/com/test/mq/rabbitmq/client/Send.java
@@ -1,2 +1,26 @@
-package com.test.mq.rabbitmq.client;public class Send {
+package com.test.mq.rabbitmq.client;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+public class Send {
+
+
+    public static void main(String[] args) throws Exception {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("locahost");
+
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare("firstQueue", false, false, false, null);
+            channel.basicPublish("firstExchange", "first.first", null, "nihao".getBytes());
+
+            System.out.println(" [x] Sent 'this is first Message'");
+        }
+
+
+    }
+
 }
