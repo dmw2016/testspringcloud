e71f3e1c114f0543e63755aa3604613a5abfd9ac
dmw2016 <2303237415@qq.com>
2020-08-17 17:23:06 +0800

    新加功能：springboot 单元测试

--- testspringcloud-web/src/test/java/org/springcloudtest/example/demo/HelloApplicationTest.java
+++ testspringcloud-web/src/test/java/org/springcloudtest/example/demo/HelloApplicationTest.java
@@ -2,20 +2,15 @@ package org.springcloudtest.example.demo;
 
 import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import javax.annotation.Resource;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -31,13 +26,13 @@ public class HelloApplicationTest {
     private MockMvc mvc;
 
 
-
     @Test
-    public void sayHello() throws Exception{
+    public void sayHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/hello/sayHello").accept(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk())
-        .andExpect(content().string(equalTo("我是中国人")));
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("我是中国人")));
     }
+
     @AfterEach
     void tearDown() {
     }
